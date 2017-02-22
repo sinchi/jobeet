cb9b9713fa52f3402b22b5e5f160df7d98b67163
ayoub belghar <ayoubbelghar@iMacdeErimaReda.Home>
2017-02-22 11:26:14 +0000

    test conflt

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -12,7 +12,7 @@ class jobActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request)
   {
-      
+
      /* $q = Doctrine_Query::create()
               ->from('JobeetJob j')
               ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()));*/
@@ -84,3 +84,5 @@ class jobActions extends sfActions
     }
   }
 }
+
+// branch master
